f547c1211cf6fb5d2ae8219a135abcca26238d3c
Malav Champaneria <68182070+Malav786@users.noreply.github.com>
2022-09-13 12:32:31 +0530

    Components Commited

--- src/main/webapps/src/components/footer.js
+++ src/main/webapps/src/components/footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
       <Navbar fixed="bottom" bg="dark" variant="dark">  
         <Container  className="text-center text-muted">
             <Col lg={12} className="text-center text-light">
-            {year} - All Rights are Reserved!!!
+            {year} - All Rights are Reserved!!! 
             </Col>
         </Container>
       </Navbar>
